6ca6152fdfc0b44516750a5bd2b61266191f1ba6
Simon Kelly <skelly@dimagi.com>
2023-08-24 18:09:59 +0200

    update test and add session

--- tests/test_celery.py
+++ tests/test_celery.py
@@ -19,60 +19,59 @@ def bind_settings():
 def test_celery_task(celery_session_app, celery_session_worker, bind_settings):
     @celery_session_app.task(bind=True, base=Task)
     def add_normal(self, a, b):
-        with mock.patch("taskbadger.celery.get_task") as get_task:
-            assert self.request.get("taskbadger_task") is None
-
-            assert self.taskbadger_task is not None
-            assert self.request.get("taskbadger_task") is not None
-            get_task.assert_called_once()
-
-            # check that the task is cached
-            assert self.taskbadger_task is not None
-            get_task.assert_called_once()
-
+        assert self.request.get("taskbadger_task") is not None
+        assert self.taskbadger_task is not None
+        assert Badger.current.session().client is not None
         return a + b
 
     celery_session_worker.reload()
 
     with mock.patch("taskbadger.celery.create_task_safe") as create, mock.patch(
         "taskbadger.celery.update_task_safe"
-    ) as update:
+    ) as update, mock.patch("taskbadger.celery.get_task") as get_task:
         result = add_normal.delay(2, 2)
         assert result.get(timeout=10, propagate=True) == 4
 
-    create.assert_called()
-    update.assert_called()
+    create.assert_called_once()
+    get_task.assert_called_once()
+    assert update.call_count == 2
+    assert Badger.current.session().client is None
 
 
 def test_celery_task_error(celery_session_app, celery_session_worker, bind_settings):
     @celery_session_app.task(bind=True, base=Task)
     def add_error(self, a, b):
-        with mock.patch("taskbadger.celery.get_task") as get_task:
-            get_task.return_value = task_for_test()
-            assert self.taskbadger_task is not None
+        assert self.taskbadger_task is not None
+        assert Badger.current.session().client is not None
         raise Exception("error")
 
     celery_session_worker.reload()
 
     with mock.patch("taskbadger.celery.create_task_safe") as create, mock.patch(
         "taskbadger.celery.update_task_safe"
-    ) as update:
+    ) as update, mock.patch("taskbadger.celery.get_task") as get_task:
+        get_task.return_value = task_for_test()
         result = add_error.delay(2, 2)
         with pytest.raises(Exception):
             result.get(timeout=10, propagate=True)
 
     create.assert_called()
-    update.assert_called_with(mock.ANY, status=StatusEnum.ERROR, data=mock.ANY)
-    data_kwarg = update.call_args_list[0][1]["data"]
+    update.assert_has_calls(
+        [
+            mock.call(mock.ANY, status=StatusEnum.PROCESSING, data=mock.ANY),
+            mock.call(mock.ANY, status=StatusEnum.ERROR, data=mock.ANY),
+        ]
+    )
+    data_kwarg = update.call_args_list[1][1]["data"]
     assert "Traceback" in data_kwarg["exception"]
+    assert Badger.current.session().client is None
 
 
 def test_celery_task_retry(celery_session_app, celery_session_worker, bind_settings):
     @celery_session_app.task(bind=True, base=Task)
     def add_retry(self, a, b, is_retry=False):
-        with mock.patch("taskbadger.celery.get_task") as get_task:
-            get_task.return_value = task_for_test()
-            assert self.taskbadger_task is not None
+        assert self.taskbadger_task is not None
+        assert Badger.current.session().client is not None
         if not is_retry:
             raise self.retry(kwargs={"is_retry": True}, max_retries=1, countdown=0)
 
@@ -82,15 +81,19 @@ def test_celery_task_retry(celery_session_app, celery_session_worker, bind_setti
 
     with mock.patch("taskbadger.celery.create_task_safe") as create, mock.patch(
         "taskbadger.celery.update_task_safe"
-    ) as update:
+    ) as update, mock.patch("taskbadger.celery.get_task") as get_task:
+        get_task.return_value = task_for_test()
         result = add_retry.delay(2, 2)
         assert result.get(timeout=10, propagate=True) == 4
 
     assert create.call_count == 2
     assert update.call_args_list == [
+        mock.call(mock.ANY, status=StatusEnum.PROCESSING, data=mock.ANY),
         mock.call(mock.ANY, status=StatusEnum.ERROR, data=mock.ANY),
+        mock.call(mock.ANY, status=StatusEnum.PROCESSING, data=mock.ANY),
         mock.call(mock.ANY, status=StatusEnum.SUCCESS, data=mock.ANY),
     ]
+    assert Badger.current.session().client is None
 
 
 def test_celery_task_badger_not_configured(celery_session_app, celery_session_worker):
@@ -98,6 +101,7 @@ def test_celery_task_badger_not_configured(celery_session_app, celery_session_wo
     def add_no_tb(self, a, b):
         with mock.patch("taskbadger.celery.get_task") as get_task:
             assert self.taskbadger_task_id is None
+            assert Badger.current.session().client is None
             get_task.assert_not_called()
 
         return a + b
@@ -112,6 +116,7 @@ def test_celery_task_badger_not_configured(celery_session_app, celery_session_wo
 
     create.assert_not_called()
     update.assert_not_called()
+    assert Badger.current.session().client is None
 
 
 def test_task_direct_call(celery_session_app, celery_session_worker):
@@ -119,6 +124,7 @@ def test_task_direct_call(celery_session_app, celery_session_worker):
     def add_direct(self, a, b):
         with mock.patch("taskbadger.celery.get_task") as get_task:
             assert self.taskbadger_task_id is None
+            assert Badger.current.session().client is None
             get_task.assert_not_called()
 
         return a + b
@@ -128,24 +134,24 @@ def test_task_direct_call(celery_session_app, celery_session_worker):
     assert add_direct(2, 1) == 3
     result = add_direct.apply(args=[2, 3])
     assert result.get(timeout=1, propagate=True) == 5
+    assert Badger.current.session().client is None
 
 
 def test_task_shared_task(celery_session_worker, bind_settings):
     @celery.shared_task(bind=True, base=Task)
     def add_shared_task(self, a, b):
-        with mock.patch("taskbadger.celery.get_task") as get_task:
-            assert self.taskbadger_task is not None
-            get_task.assert_called()
-
+        assert self.taskbadger_task is not None
+        assert Badger.current.session().client is not None
         return a + b
 
     celery_session_worker.reload()
 
     with mock.patch("taskbadger.celery.create_task_safe") as create, mock.patch(
         "taskbadger.celery.update_task_safe"
-    ) as update:
+    ) as update, mock.patch("taskbadger.celery.get_task") as get_task:
         result = add_shared_task.delay(2, 2)
         assert result.get(timeout=10, propagate=True) == 4
 
     create.assert_called()
     update.assert_called()
+    assert Badger.current.session().client is None
